293dd7da7a70c1b6e03ef18f529f44ce710b2d16
d-yerovi <day5@njit.edu>
2024-06-08 19:21:42 -0400

    making last changes to fix error while running pytest

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,5 +1,30 @@
-def add(a,b):
-    return a + b
+from calculator.calculations import Calculations
+from calculator.operations import add, subtract, multiply, divide
+from calculator.calculation import Calculation 
+from decimal import Decimal
+from typing import Callable
 
-def subtract(a,b):
-    return a - b
+class Calculator:
+    @staticmethod
+    def _perform_operation(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal],Decimal]) -> Calculation:
+        '''perform a calculation, return result.'''
+        calculation = Calculation.insert_operation(a, b, operation)
+        Calculations.add_calculation (calculation) # Assuming add_calculation is a method
+        return calculation.perform()
+    
+    @staticmethod
+    def add(a: Decimal, b: Decimal) -> Decimal:
+        return Calculator._perform_operation(a,b, add)
+
+    @staticmethod
+    def subtract(a: Decimal, b: Decimal) -> Decimal:
+        return Calculator._perform_operation(a,b, subtract)
+
+
+    @staticmethod
+    def multiply(a: Decimal, b: Decimal) -> Decimal:
+        return Calculator._perform_operation(a,b, multiply)
+    
+    @staticmethod
+    def divide(a: Decimal, b: Decimal) -> Decimal:
+        return Calculator._perform_operation(a,b, divide)
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,10 +1,18 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+from calculator import Calculator
 
 def test_addition():
     '''Test that addition function works '''
-    assert add(2,2) == 4
+    assert Calculator.add(2,2) == 4
 
 def test_subtraction():
     '''Test that addition function works '''
-    assert subtract(2,2) == 0
+    assert Calculator.subtract(2,2) == 0
+
+def test_multiplication():
+    '''test multiplication'''
+    assert Calculator.multiply(2,2) == 4
+
+def test_divide():
+    '''test division'''
+    assert Calculator.divide(2,2) == 1
