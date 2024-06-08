91fa6393ec5334211e36d562f7fde04d71f34c14
d-yerovi <day5@njit.edu>
2024-06-08 19:14:09 -0400

    adding require files to make app to work

---
+++ calculator/calculations.py
@@ -0,0 +1,33 @@
+from decimal import Decimal
+from typing import Callable, List
+from calculator.calculation import Calculation
+
+class Calculations:
+    history: List[Calculation] = []
+
+    @classmethod
+    def add_calculation(cls, calculation: Calculation):
+        """Add calculation to the history"""
+        cls.history.append(calculation) 
+    
+    @classmethod
+    def get_history(cls) -> List[Calculation]:
+        '''Retrieve entire history of calculations'''
+        return cls.history 
+    
+    @classmethod
+    def clear_history(cls):
+        '''Clear the historv of calculations'''
+        cls.history.clear()
+
+    @classmethod
+    def get_latest(cls) -> Calculation:
+        '''get the latest calculation or return none if there is no history'''
+        cls._latest = cls.history[-1]
+        if cls._latest: return cls._latest 
+        else: return None
+
+    @classmethod 
+    def search_operation(cls, operation_name: str) -> List[Calculation]:
+        '''seach for calculation by operation name'''
+        return [calc for calc in cls.history if calc.operation_name == operation_name]
---
+++ tests/test_calculation.py
@@ -0,0 +1,36 @@
+'''This module contains tests for the calculator operations and Calculation class.'''
+# pylint: disable=unnecessary-dunder-call, invalid-name
+from decimal import Decimal
+import pytest
+from calculator.calculation import Calculation
+from calculator.operations import add, subtract, multiply, divide
+
+# It's good practice to add a docstring at the beginning of the module
+
+@pytest.mark.parametrize("a, b, operation, expected", [
+    (Decimal('10'), Decimal('5'), add, Decimal('15')),
+    (Decimal ('10'), Decimal('5'), subtract, Decimal('5')),
+    (Decimal ('10'), Decimal('5'), multiply, Decimal ('50')),
+    (Decimal ('10'), Decimal('2'), divide, Decimal('5')),
+    (Decimal ('10.5'), Decimal('0.5'), add, Decimal('11.0')),
+    (Decimal('10.5'),Decimal ('0.5'), subtract, Decimal ('10.0')),
+    (Decimal ('10.5'), Decimal('2'), multiply, Decimal ('21.0')),
+    (Decimal('10'), Decimal('0.5'), divide, Decimal('20')),
+])
+
+def test_calculation_operation(a, b, operation, expected):
+    '''test calculation with various cases'''
+    calc = Calculation(a, b, operation)
+    assert calc.perform() == expected, f'Fail {operation.__name__} operation with values {a}, {b}.'
+
+def test_calculation_repr():
+    '''check the string format of the calculation class'''
+    calc = Calculation(Decimal("10"), Decimal("5"), add)
+    expected_repr = "calculation(10, 5, add)"
+    assert calc.__repr__() == expected_repr, "The (__repre__) method output doesn't match operation"
+
+def test_divide_by_zero():
+    '''test division method divided by zero to make sure it raises a ValueError'''
+    calc = Calculation(Decimal("10"), Decimal("0"), divide)
+    with pytest.raises(ValueError, match='Error, cannot divide number by zero'):
+        calc.perform()
