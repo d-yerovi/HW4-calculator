501272735c539a5122f418e009dfdd94f630150a
d-yerovi <day5@njit.edu>
2024-06-11 00:36:00 -0400

    installed faker and added to requirements

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -4,14 +4,14 @@ from decimal import Decimal
 from calculator.calculation import Calculation
 from calculator.operations import add, subtract, multiply, divide
 
-
+""""""
 def test_operation_add():
     '''Testing the addition operation'''
     calculation = Calculation (Decimal('10'), Decimal('5'), add)
     assert calculation.perform() == Decimal('15'), "Add operation failed"
 
 def test_operation_subtract():
-    "''Testing the subtract operation'"
+    '''Testing the subtract operation'''
     calculation = Calculation (Decimal('10'), Decimal('5'), subtract)
     assert calculation.perform() == Decimal('5'), "Subtract operation failed"
 
